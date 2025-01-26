a0c6b10184a63b16ff2f570e20a0e52e82724a03
temochka2005 <glaid2004@gmail.com>
2025-01-26 18:19:42 +0300

    temporary version with bug in sliding window

--- server_graphics.py
+++ server_graphics.py
@@ -1,6 +1,6 @@
 import dash
 from dash import dcc, html, no_update
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
 
 from threading import Thread
@@ -25,33 +25,46 @@ class DashBuffer(Buffer):
         t.start()
 
     def make_layout(self):
-        """
-        Макет приложения
-        """
         self.app.layout = html.Div(children=[
             dcc.Store(id='data-store', storage_type='session'),
+            dcc.Store(id='last-update'),
+            
             html.Div([
                 html.H1(children='Server_graphics SNcoincidence test')
             ]),
-
+            
+            # Блок с графиком и интервалами
             html.Div([
                 dcc.Graph(id='box-plots'), 
                 dcc.Interval(
-                    id='interval-component', 
+                    id='interval-component_data', 
                     interval=500,  # Обновление каждые 0.5 сек
+                    n_intervals=0
+                ),
+                dcc.Interval(
+                    id='interval-component_ui', 
+                    interval=100,  # Обновление каждые 0.1 сек
                     n_intervals=0
                 )
             ]),
-
+            
+            # Добавляем кнопку (уже была) и ниже - наш переключатель
             html.Div([
                 html.Button(
                     id='data_update_button', 
                     n_clicks=0, 
                     children='Update'
+                ),
+                html.Label("Скользящее окно (48 ч):"),
+                dcc.Checklist(
+                    id='sliding-window-toggle',
+                    options=[{'label': 'Вкл', 'value': 'ON'}],
+                    value=[],  # изначально переключатель выключен
+                    labelStyle={'display': 'inline-block', 'marginLeft': '10px'}
                 )
             ])
-
         ])
+
         
     def define_callbacks(self):
         """
@@ -59,15 +72,13 @@ class DashBuffer(Buffer):
         """
         self.app.callback(
             Output('data-store', 'data'),
-            [
-                Input('data_update_button', 'n_clicks'),
-                Input('interval-component', 'n_intervals')
-            ]     
+            Input('interval-component_data', 'n_intervals'),     
         )(self.update_data)
 
         @self.app.callback(
-            Output('box-plots', 'figure'),
+            Output('box-plots', 'figure', allow_duplicate=True),
             Input('data-store', 'data'),
+            prevent_initial_call=True
         )
         def update_graph(data: list[dict]):
             if data == {}:
@@ -75,14 +86,11 @@ class DashBuffer(Buffer):
             
             # Список для хранения "ящиков" (линий) для каждого userID
             box_plots = []
-
+            # current_time = datetime.datetime.now().timestamp()
             # Преобразуем float-значения времени в datetime и формируем график
             for datablock in data:
                 # <-- (Новая секция) преобразуем ts в datetime:
-                x_vals = [
-                    datetime.datetime.fromtimestamp(ts_val) 
-                    for ts_val in datablock["ts"]
-                ]
+                x_vals = list(map(datetime.datetime.fromtimestamp, datablock["ts"]))
                 box_plots.append(go.Scatter(
                     x=x_vals,
                     y=datablock["zs"],
@@ -91,10 +99,11 @@ class DashBuffer(Buffer):
 
             # Создание фигуры
             fig = go.Figure(data=box_plots)
-
             # Настройка осей и заголовков
             fig.update_xaxes(
-                tickformat='%Y-%m-%d %H:%M:%S'  # <-- (Новая строка) формат оси X
+                tickformat='%Y-%m-%d %H:%M:%S',
+                range=[None, 0],
+                autorange=True  # <-- (Новая строка) формат оси X
             )
             fig.update_layout(
                 uirevision='constant',      # <-- (Новая строка) чтобы масштаб не сбрасывался
@@ -104,7 +113,53 @@ class DashBuffer(Buffer):
             )
 
             return fig
-    
+
+        @self.app.callback(
+            [   # Выходы
+                Output('box-plots', 'figure'),
+                Output('last-update', 'data')
+            ],
+            [   # Входы
+                Input('interval-component_ui', 'n_intervals'),
+                Input('sliding-window-toggle', 'value')
+            ],
+            [   # Состояния
+                State('box-plots', 'figure'),
+                State('last-update', 'data')
+            ]
+        )
+        def update_ui(_, toggle_value, figure, last_update):
+            """
+            Колбэк, который раз в 0.1 сек обновляет ось X, если включён режим 
+            "скользящее окно".
+            """
+            current_time = datetime.datetime.now().timestamp()
+            
+            # Если это первое срабатывание -- просто зафиксируем время.
+            if last_update is None:
+                return figure, current_time
+            
+            # Проверяем, включён ли наш переключатель.
+            # Если в toggle_value лежит 'ON', значит пользователь включил "скользящее окно".
+            if 'ON' in toggle_value:
+                # Выключаем autorange:
+                figure['layout']['xaxis']['autorange'] = False
+                
+                # Задаём правую границу как "текущее время", а левую - за 48 ч до него.
+                tmin = datetime.datetime.fromtimestamp(current_time - 15)
+                tmax = datetime.datetime.fromtimestamp(current_time)
+                
+                # Прописываем фиксированный диапазон [tmin, tmax].
+                figure['layout']['xaxis']['range'] = [tmin, tmax]
+            else:
+                # Возвращаемся к режиму авторесайза по X:
+                figure['layout']['xaxis']['autorange'] = True
+                # Удаляем ключ 'range', чтобы plotly не оставлял старое значение.
+                if 'range' in figure['layout']['xaxis']:
+                    del figure['layout']['xaxis']['range']
+
+            return figure, current_time
+
     # <-- (Новая функция) функция, которая удаляет из self.clients данные старше self.drop_tail_hours
     def drop_tail(self):
         """Удаляем данные старше заданного количества часов."""
